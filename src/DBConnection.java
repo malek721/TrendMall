ee6ed775b68c03f9f04da06be644ba7df8d22da5
Amr Walidi <amr.nawaf@gmail.com>
2023-12-22 19:00:12 +0300

    Add Database Connection

---
+++ src/DBConnection.java
@@ -0,0 +1,32 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnection {
+    private final static String database = "dblab";
+    private final static String username = "postgres";
+    private final static String password = "AmrF.C.B";
+    private static Connection conn = null;
+    private static final DBConnection dbconn = new DBConnection();
+
+    private DBConnection() {
+        try {
+            Class.forName("org.postgresql.Driver");
+            conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/" + database, username, password);
+            if (conn != null) {
+                System.out.println("Connection established");
+            } else {
+                System.out.println("Connection Failed");
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public static DBConnection getInstance() {
+        return dbconn;
+    }
+
+    public Connection getConnection() {
+        return conn;
+    }
+}
