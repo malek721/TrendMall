5e1b4aeabdccdec44fb61576c0ffda4243ccc928
malek <malekco721@gmail.com>
2023-12-27 13:12:37 +0300

    ProfileDuzenleme sayfasi olusturma

---
+++ src/ProfileDuzenleme.java
@@ -0,0 +1,18 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ProfileDuzenleme implements ActionListener {
+
+
+
+
+
+
+
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
