57264510ae14fec3f4b37e6e09db40ee62195d2c
malek <malekco721@gmail.com>
2023-12-27 17:38:08 +0300

    ProfileDuzenleme sayfasi olusturma

--- src/ProfileDuzenleme.java
+++ src/ProfileDuzenleme.java
@@ -1,10 +1,23 @@
+import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class ProfileDuzenleme implements ActionListener {
+        JPanel panUp,panDown;
+        JButton cikisBtn,donBtn,kayidetBtn;
+        ImageIcon logo;
+        JLabel imageContainer,adLb,soyadLb,tel_noLb,adresLb,sifreLb;
+        TrendMallFrame profileDuzenlemeFrame;
+
+        public ProfileDuzenleme(){
 
 
 
+
+
+
+
+        }
 
 
 
